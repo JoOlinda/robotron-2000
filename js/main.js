9346b72ce78c90080e46c6ca2a5179e7b380cbe0
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-28 23:21:20 -0300

    Add evento para manipular valor do braço

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("oi")
+let cr7 = document.querySelector('.robo')
+const controles = document.querySelectorAll('.controle-ajuste') 
+const braco = document.querySelector('#braco')
+
+cr7.addEventListener('click', ()=>{
+    console.log('Fui clicado')
+})
+controles.forEach(element => {
+    element.addEventListener('click', (event)=>{
+        manipulaDados(event.target.textContent)
+    })
+});
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
+    
+}
